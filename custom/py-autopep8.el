e6328299a96f6c2c7d10f2829658b979e4959350
dongweiming <dongweiming@douban.com>
2015-01-06 11:20:35 +0800

    modify autopep8

--- custom/py-autopep8.el
+++ custom/py-autopep8.el
@@ -99,7 +99,7 @@ Note that `--in-place' is used by default."
       (erase-buffer))
     (write-region nil nil tmpfile)
     (if (zerop (apply 'call-process "autopep8" nil errbuf nil
-                      (append `("--in-place" ,tmpfile) py-autopep8-options)))
+                      (append `("--in-place" "--ignore=E24,E309",tmpfile) py-autopep8-options)))
         (if (zerop (call-process-region (point-min) (point-max) "diff" nil patchbuf nil "-n" "-" tmpfile))
             (progn
               (kill-buffer errbuf)
